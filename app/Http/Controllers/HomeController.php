f78a94d90dc7d19715183a22cfb99b5fd12e307d
mizanur <mizanurahmed1452@gmail.com>
2020-03-01 12:20:35 +0600

    Home Page done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        $data['featured_posts']=Post::where('is_featured', 1)->where('status', 'published')->get();
+        $data['featured_posts']=Post::with(['category', 'author'])->Published()->where('is_featured', 1)->get();
+        $data['latest_posts']=Post::with(['category', 'author'])->Published()->orderBy('id', 'desc')->paginate(6);
+        $data['popular_posts']=Post::Published()->orderBy('total_hit', 'desc')->limit(3)->get();
+        $data['categories']=Category::all();
         return view('front.index', $data);
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -23,4 +23,7 @@ class Post extends Model
     public function author(){
         return $this->belongsTo(Author::class);
     }
+    public function scopePublished($query){
+       return $query->where('status', 'published');
+    }
 }
